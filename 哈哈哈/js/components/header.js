2215b78c8d1b317b2b08fc4c403c383a18036cff
Wang-xuan-yu <67509498+Wang-xuan-yu@users.noreply.github.com>
2020-09-01 08:55:07 +0800

    wxy

---
+++ 哈哈哈/js/components/header.js
@@ -0,0 +1,35 @@
+document.write(`
+<header class="container clearfix">
+    <!--logo-->
+    <h1 class="logo pull-left">
+        <a href="index.html" title="朗朗口腔">
+            <img src="img/logo.png" alt="logo"/>
+        </a>
+    </h1>
+    <div class="pull-right">
+        <!--导航栏-->
+        <nav>
+            <ul class="clearfix">
+                <li class="current">
+                    <a href="index.html" title="首页">首页</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" title="走进朗朗">走进朗朗</a>
+                </li>
+                <li>
+                    <a href="news.html" title="资讯动态">资讯动态</a>
+                </li>
+                <li>
+                    <a href="partner.html" title="合作伙伴">合作伙伴</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" title="辅助功能">辅助功能</a>
+                </li>
+                <li>
+                    <a href="contact.html" title="联系我们">联系我们</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
+`)
